41cdce16ba7491883eeeb9a42d85c152579395d5
thio.perdana <thio.perdana@outlook.com>
2025-11-26 14:09:29 +0700

    feat: Add CORS middleware to allow requests from notispaces.cloud domains.

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ import json
 from pydantic import BaseModel
 from typing import List, Optional
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://notispaces.cloud",
+    "https://notispaces.cloud"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Database
 sqlite_file_name = "data/astro.db"
